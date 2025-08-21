1138e6a8137e238aaa76ee9b105b738df4a98b35
Andrey <osorioandrey97@gmail.com>
2025-08-21 07:48:49 -0500

    conexión

--- files/backend/db.js
+++ files/backend/db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  password: process.env.DB_NAME,
   multipleStatements: true, // opcional
 }).promise();
 
